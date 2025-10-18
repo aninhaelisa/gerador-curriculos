504af86f84a501dad8e8e7186cb478dee878ee9a
aninhaelisa <elisaana.2019.2006@gmail.com>
2025-10-18 13:41:05 -0300

    refatoração e correção do código de envio de currículos

--- gerar_curriculo.php
+++ gerar_curriculo.php
@@ -1,23 +1,23 @@
 <?php
- if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $uploadDir = 'uploads/';
-    if (!is_dir(filename: $uploadDir)) {
-        mkdir(directory: $uploadDir,0775, true);
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir,0755, true);
     }
 
     $fotoPath = '';
 
-    if (isset($_FILE['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK){
+    if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK){
         $allowedTypes = ['image/jpeg','image/png','image/gif'];
         $fileType = $_FILES['foto']['type'];
 
-        if (in_array(needle: $fileType, haystack: $allowedTypes)){
-            $extension = pathinfo(path: $_FILE['foto']['name'], flags: PATHINFP_EXTENSION);
-            $filename = uniqid() . '.' . $extesion;
+        if (in_array( $fileType, $allowedTypes)){
+            $extension = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+            $filename = uniqid() . '.' . $extension;
             $fotoPath = $uploadDir . $filename;
 
-            if (!move_uploaded_file(from: $_FILES['foto']['tmp_name'], to: $fotoPath)){
+            if (!move_uploaded_file( $_FILES['foto']['tmp_name'],  $fotoPath)){
                 $fotoPath = '';
             }
         }
@@ -44,5 +44,10 @@
         'referencias' => $_POST['referencias'] ?? []
     ];
 
-    
+    include 'curriculo_template.php';  
+ } else {
+    header('Location: index.php');
+    exit;
  }
+
+?>
