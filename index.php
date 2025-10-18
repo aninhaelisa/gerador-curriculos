b53243130e7737d8f683589c03bbb71374975b31
aninhaelisa <elisaana.2019.2006@gmail.com>
2025-10-18 10:42:08 -0300

    Inicio class Container

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
     <link rel="shortcut icon" href="assets/cv.png" type="image/x-icon">
 </head>
 <body>
+
+<div class="container mt-4">
+
+<h1 class="text-center mb-4"> Crie Seu Currículo Profissional</h1>
+
+
+</div>
+
+
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="js/script.js"></script>
